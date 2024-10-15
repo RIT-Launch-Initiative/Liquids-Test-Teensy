a65376656ba09ef40b806a843074731f8443d2ff
T0ucan <181164199+T0ucan-png@users.noreply.github.com>
2024-10-15 19:30:32 -0400

    The actual code I meant to commit. Oops.

--- Servo_control/Servo_control.ino
+++ Servo_control/Servo_control.ino
@@ -1,56 +1,99 @@
-//#include <PWMServo.h>
+#include <PWMServo.h>
 #include <SPI.h>
 #include <NativeEthernet.h>
-
 #include <NativeEthernetUdp.h>
+//--------------------------------------------------------------
+//Declare any globally important values
+//Servo-related Values
+PWMServo Chester;
+int currentAng;
 
+//Ethernet stuff
+byte mac[] = {
+  0x04, 0xE9, 0xE5, 0x11, 0xEF, 0x6E      // Set the Mac Address
+};
+byte ip[] = {10, 10, 10, 75};
+byte dns[] = {10, 10, 10, 1};
+byte gateway[] = {169, 254, 147, 58};
+byte subnet[] = {255, 255, 255, 0};
+unsigned int localPort = 8080;
+EthernetUDP data;
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE];
 
-
-// Enter a MAC address and IP address for your controller below.
-
-// The IP address will be dependent on your local network:
-
-byte mac[] = {  
-
-  0x04, 0xE9, 0xE5, 0x11, 0xEF, 0x6E };
-
-byte ip[] = {129, 69, 69, 69};
-  IPAddress balls(10, 10, 10, 70);
-
-
-unsigned int localPort = 8888;      // local port to listen on
-
-
-
-// An EthernetUDP instance to let us send and receive packets over UDP
-
-EthernetUDP Udp;
-
-
+//Packet-related values
+uint16_t heartbeatCount = 0; //Amount of heartbeats
+uint16_t beatTimer; //Timer to send out heartbeat packet
+//String heartData = /*Decide what to send as heartbeat*/;
+int dataLength;
+//--------------------------------------------------------------
+// Arduino setup for servo comms and ethernet data reading
 
 void setup() {
+  //Begin Ethernet 
+  Ethernet.begin(mac, ip, dns, gateway, subnet);
 
-  // start the Ethernet and UDP:
+//Start the serial communications
   Serial.begin(38400);
+  while(!Serial){
+    ; //Wait for serial to connect
+  }
+  
+//attach the servo, named Chester
+  //Chester.attach(3);
 
-  Ethernet.begin(mac,ip);
-
-  Udp.begin(localPort);
-
-
-
+//Begin UDP connection
+  data.begin(localPort);
 }
-
-
+//--------------------------------------------------------------
+// Main loop
 void loop() {
-  do{
-    Serial.println(Ethernet.hardwareStatus());
-    if (Udp.beginPacket(balls, 8888)){
-      Udp.beginPacket(balls, 8888);
-      Udp.write("lol");
-      Udp.endPacket();
-    }else{
-      Serial.printf("lol");
+    //currentAng = Chester.read();
+    //Check for packet------------------
+    dataLength = data.parsePacket();
+    if (dataLength){
+      data.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+      Serial.print("Received packet of size ");
+      Serial.println(dataLength);
+      Serial.print("From ");
+      IPAddress remote = data.remoteIP();
+      for (int i =0; i < 4; i++){
+        Serial.print(remote[i], DEC);
+        if (i < 3){
+          Serial.print(".");
+        }
+      }
+      Serial.print(", port ");
+      Serial.println(data.remotePort());
+
+      // read the packet into packetBuffer
+      data.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+      Serial.println("Contents:");
+      Serial.println(packetBuffer);
+
+      data.beginPacket(data.remoteIP(), data.remotePort());
+      data.write("acknowldeged");
+      data.endPacket();
+
+      //beatTimer++;
+      //if(beatTimer >= /*Some amount of seconds*/){
+        //Write heartbeat packet and send it
+      //  delay(/*Scary time for heartbeat retrieveal*/)
+        //Check the data sent back and compare to what was sent
+      //  data.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+      //  if(packetBuffer == heartData){
+      //    heartbeatCount++;
+      //  }else{ 
+      //    if(currentAng != 0){ //Check if open or closed
+      //      Chester.write(0);
+            //Wait indefinitely, will only do something
+            //once turned off, then on again.
+      //      for(;;){}
+      //    }else{
+      //      for(;;){}
+      //    }
+      //  }
+      //}
     }
-  }while(1);
+    //end packet check-----------------
+  delay(10);
 }
